0b795bb4a43696fb838098f2cd0a7385eb5d56bd
arkas80 <souzitsouzi20@gmail.com>
2026-04-03 22:46:36 +0300

    Add configuration settings for ESP32-S3

---
+++ config.h
@@ -0,0 +1,25 @@
+// config.h
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi settings
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+
+// ThingsSpeak settings
+#define THINGSPEAK_API_KEY "your_thingspeak_api_key"
+
+// Sensor pins
+#define TEMP_SENSOR_PIN 34
+#define HUMIDITY_SENSOR_PIN 35
+#define LIGHT_SENSOR_PIN 32
+
+// Power management settings
+#define POWER_MANAGEMENT_ENABLED true
+#define POWER_SAVE_MODE_ENABLED true
+
+// ESP32-S3 settings
+#define ESP32_S3_FEATURE_X_ENABLED true
+
+#endif // CONFIG_H
